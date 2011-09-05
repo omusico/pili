8d379083a0215264b1bcc96723e68b32272580ff
Girvan Liu <girvanliu@gmail.com>
2011-09-05 22:46:02 +0800

    add i18n

---
+++ i18n/__init__.py
@@ -0,0 +1,19 @@
+import config
+
+class I18n():
+    def __init__(self, lang='en-US', path='i18n', type_='file'):
+        self.lang = lang
+        self.path = path
+        self.type_ = type_
+
+    def get(self, key):
+        if self.type_ == 'file':
+            return self._get_file(key)
+
+    def _get_file(self, key):
+        key = key.replace('/', '.')
+        if '.' in key:
+            module, obj = key.rsplit('.', 1)
+            module = self.path + '.' + module + '.' + self.lang.replace('-','_')
+            return getattr(__import__(module, None, None, [obj]), obj)
+
